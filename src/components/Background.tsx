0d4508f41fbaecbe99fa43a88e9110dc5e5756fd
well1403w <carlosvarao.frontend@hotmail.com>
2025-11-04 22:47:02 -0300

    ajustes no header

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -8,9 +8,11 @@ export default function Background({ children }: BackgroundProps) {
       <div className="bg-[#0A0A1A] font-bold w-screen h-screen">
         <div className="fixed top-0 left-0 w-full h-full  overflow-hidden">
           <div className="absolute top-[-20%] left-[-20%] w-[500px] h-[500px] bg-linear-to-br from-blue-500/30 to-purple-600/30 rounded-full filter blur-3xl animate-pulse"></div>
-          <div className="w-full  flex flex-col justify-center items-center">
-            <div className="min-w-[1200px]">{children}</div>
+
+          <div className="relative flex flex-col items-center w-full pt-4">
+            <div className="w-full max-w-[1200px] px-4 sm:px-6">{children}</div>
           </div>
+
           <div className="absolute bottom-[-10%] right-[-10%] w-[600px] h-[600px] bg-linear-to-tl from-cyan-400/30 to-indigo-500/30 rounded-full filter blur-3xl animate-pulse animation-delay-3000"></div>
         </div>
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,49 +1,134 @@
+import { useState } from "react";
 import { BsFillTerminalFill } from "react-icons/bs";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { ImExit } from "react-icons/im";
+//import { IoIosCloseCircle } from "react-icons/io";
 
-export default function header() {
+export default function Header() {
+  const [clickSidbar, setClickSidbar] = useState(false);
+
+  function clickSidbarExit(e: React.MouseEvent<HTMLDivElement>) {
+    if (e.target === e.currentTarget) {
+      setClickSidbar(false);
+    }
+    console.log(e);
+  }
+
   return (
     <>
-      <div className="sticky top-4 z-50 flex items-center justify-between whitespace-nowrap rounded-xl px-6 py-3 my-4 glass-effect shadow-lg text-[#e2e8f0]">
-        <div className="flex items-center gap-4 text-inter">
-          <BsFillTerminalFill color="#22d3ee" fontSize={25} />
-          <h2 className="font-display text-lg font-bold leading-tight tracking-[-0.015em]">
-            Carlos Varão
-          </h2>
-        </div>
-        <nav className="hidden md:flex flex-1 justify-end items-center gap-8 text-Space">
-          <div className="flex items-center gap-6">
-            <a
-              className="font-display text-sm font-medium leading-normal text-text-light hover:text-accent-cyan transition-colors "
-              href="#sobre"
-            >
-              Sobre
-            </a>
-            <a
-              className="font-display text-sm font-medium leading-normal text-text-light hover:text-accent-cyan transition-colors"
-              href="#projetos"
-            >
-              Projetos
-            </a>
-            <a
-              className="font-display text-sm font-medium leading-normal text-text-light hover:text-accent-cyan transition-colors"
-              href="#habilidades"
-            >
-              Habilidades
-            </a>
-            <a
-              className="font-display text-sm font-medium leading-normal text-text-light hover:text-accent-cyan transition-colors"
-              href="#contato"
-            >
-              Contato
-            </a>
+      <div className="sticky top-2 sm:top-4 z-50 w-full">
+        <div className="flex items-center justify-between rounded-xl bg-[#ffffff0d] backdrop-blur-md border border-[#ffffff1a] shadow-lg text-[#e2e8f0] font-primary px-3 py-3 ">
+          {/* Logo + Nome */}
+          <div className="flex items-center gap-2 sm:gap-3 lg:gap-4">
+            <BsFillTerminalFill className="text-[#22d3ee]" size={18} />
+            <h2 className="text-xs sm:text-sm md:text-base lg:text-lg font-bold tracking-[-0.015em]">
+              Carlos Varão
+            </h2>
           </div>
-          <button className="flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 glass-effect text-accent-cyan text-sm font-bold leading-normal tracking-[0.015em] hover:bg-accent-cyan/10 transition-colors">
-            <span className="truncate text-[#22d3ee] text-[13px] ">
-              Baixar CV
-            </span>
+
+          {/* Menu Desktop */}
+          <nav className="hidden md:flex items-center gap-6 lg:gap-8">
+            <div className="flex items-center gap-4 lg:gap-6 font-secondary">
+              <a
+                className="text-sm font-medium transition-colors hover:text-[#22D3EE]"
+                href="#sobre"
+              >
+                Sobre
+              </a>
+              <a
+                className="text-sm font-medium transition-colors hover:text-[#22D3EE]"
+                href="#projetos"
+              >
+                Projetos
+              </a>
+              <a
+                className="text-sm font-medium transition-colors hover:text-[#22D3EE]"
+                href="#habilidades"
+              >
+                Habilidades
+              </a>
+              <a
+                className="text-sm font-medium transition-colors hover:text-[#22D3EE]"
+                href="#contato"
+              >
+                Contato
+              </a>
+            </div>
+
+            <button className="flex items-center justify-center h-9 lg:h-10 px-3 lg:px-4 rounded-lg bg-[#ffffff0d] backdrop-blur-md border border-[#ffffff1a] text-xs sm:text-sm font-bold transition-all duration-300 hover:bg-[#22D3EE]/15 hover:-translate-y-0.5">
+              <span className="truncate text-[#22d3ee] text-[13px] lg:text-[14px]">
+                Baixar CV
+              </span>
+            </button>
+          </nav>
+
+          {/* Menu Mobile */}
+          <button className="md:hidden">
+            <HiOutlineMenuAlt3
+              className="text-[#22d3ee] cursor-pointer"
+              size={26}
+              onClick={() => setClickSidbar((prev) => !prev)}
+            />
           </button>
-        </nav>
+        </div>
       </div>
+
+      {clickSidbar && (
+        <div
+          className="fixed inset-0 z-99 bg-[#0A0A1A] text-white flex items-center justify-center overflow-hidden"
+          onClick={clickSidbarExit}
+        >
+          <div
+            className="w-full flex flex-col items-center justify-center gap-14"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <ul className="flex flex-col gap-8 text-center w-full px-6">
+              <li className="w-full">
+                <a
+                  href="#sobre"
+                  className="flex items-center justify-center gap-4 rounded-lg py-4 text-3xl font-semibold text-white bg-primary/20 transition-colors hover:bg-primary/30"
+                >
+                  Home
+                </a>
+              </li>
+
+              <li className="w-full">
+                <a
+                  href="#projetos"
+                  className="flex items-center justify-center gap-4 rounded-lg py-4 text-3xl font-semibold text-slate-400 transition-colors hover:text-white hover:bg-white/5"
+                >
+                  Projetos
+                </a>
+              </li>
+
+              <li className="w-full">
+                <a
+                  href="#habilidades"
+                  className="flex items-center justify-center gap-4 rounded-lg py-4 text-3xl font-semibold text-slate-400 transition-colors hover:text-white hover:bg-white/5"
+                >
+                  Habilidades
+                </a>
+              </li>
+
+              <li className="w-full">
+                <a
+                  href="#contato"
+                  className="flex items-center justify-center gap-4 rounded-lg py-4 text-3xl font-semibold text-slate-400 transition-colors hover:text-white hover:bg-white/5"
+                >
+                  Contato
+                </a>
+              </li>
+            </ul>
+
+            <ImExit
+              className="cursor-pointer hover:scale-110 transition-transform duration-200"
+              size={30}
+              color="#ff4040"
+              onClick={() => setClickSidbar((prev) => !prev)}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 }
