b762da2878f6dd9e9ac5056020bfe6407e3d31f1
Carlos Varão <carlosvarao.frontend@hotmail.com>
2025-11-10 03:20:16 -0300

    Reapply "ajustes"
    This reverts commit 1ce4941105a01d94f2dbcb47ad61cecef710d775.

--- src/services/servicesApi.ts
+++ src/services/servicesApi.ts
@@ -1,5 +1,5 @@
 import emailjs from "@emailjs/browser";
-import axios from "axios";
+//import axios from "axios";
 
 interface Data {
   remetente: string;
@@ -27,12 +27,12 @@ export async function enviarEmail(data: Data) {
   }
 }
 
-export async function enviarCadastroGit(data: Data) {
-  try {
-    const response = await 
-    return response;
-  } catch (error) {
-    console.error("Erro ao enviar e-mail:", error);
-    throw error;
-  }
-}
+// export async function enviarCadastroGit(data: Data) {
+//   try {
+//     const response = await
+//     return response;
+//   } catch (error) {
+//     console.error("Erro ao enviar e-mail:", error);
+//     throw error;
+//   }
+// }
