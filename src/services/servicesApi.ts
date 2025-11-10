1ce4941105a01d94f2dbcb47ad61cecef710d775
Carlos Varão <carlosvarao.frontend@hotmail.com>
2025-11-10 03:17:23 -0300

    Revert "ajustes"
    This reverts commit c831414bcd9811aa5583c0ce72b7fad975fcf1ef.

--- src/services/servicesApi.ts
+++ src/services/servicesApi.ts
@@ -27,29 +27,12 @@ export async function enviarEmail(data: Data) {
   }
 }
 
-export async function enviarCadastroGit(file: File | null) {
-  if (!file) throw new Error("Arquivo inválido ou inexistente.");
-
-  const repo = "new_portfolio";
-  const owner = "CarlosVarao";
-  const path = "package.json";
-  const token =
-    "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";
-
+export async function enviarCadastroGit(data: Data) {
   try {
-    const response = await axios.get(
-      `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-      {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-      }
-    );
-
-    console.log(response);
+    const response = await 
     return response;
   } catch (error) {
-    console.error("Erro ao enviar dados:", error);
+    console.error("Erro ao enviar e-mail:", error);
     throw error;
   }
 }
