1ba974e0c29c66e1084046ac5281ec0f3cd5ea84
Carlos varao <carlosvarao.frontend@hotmail.com>
2025-11-11 17:57:13 -0300

    add music e play

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import SectionProject from "../components/SectionProject";
 import SectionSkills from "../components/SectionSkills";
 import SectionContacts from "../components/SectionContacts";
 import Footer from "../components/Footer";
+import LofiPlayer from "../components/LofiPlayer";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Background header={
         <>    <Header /></>
       }>
+        <LofiPlayer />
         <SectionInicial />
         <SectionAbout />
         <SectionProject />
