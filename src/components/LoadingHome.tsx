cebc24453501faca428f06d6f9b2206eac2f684e
Carlos Varão <carlosvarao.frontend@hotmail.com>
2025-11-13 01:13:22 -0300

    add

---
+++ src/components/LoadingHome.tsx
@@ -0,0 +1,11 @@
+import iconLoadingHome from "../../public/iconLoadingHome.png";
+
+export default function LoadingHome() {
+  return (
+    <>
+      <div>
+        <img src={iconLoadingHome} alt="" />
+      </div>
+    </>
+  );
+}
